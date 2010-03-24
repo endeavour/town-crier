56bccf1b0d1618a33df2c03b8bb796d60c08ce4b
James Freiwirth <james@ciseware.com>
2010-03-24 23:28:35 +0000

    Integration test

---
+++ EmailTemplating.Tests/IntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using Ciseware.EmailTemplating;
+using NUnit.Framework;
+
+namespace EmailTemplating.Tests
+{
+    [TestFixture]
+    public class IntegrationTests
+    {
+
+        [Test]
+        public void CanSendHtmlEmail()
+        {
+            var factory =new MergedEmailFactory(new TemplateParser());
+
+            var tokenValues = new Dictionary<string,string>()
+            {{"name", "James"},
+            {"userid", "123"}};
+
+            var message = factory.CreateFromFile("Hello {%=name%}", @"templates\sample-email.html", tokenValues);
+
+            var from = new MailAddress("info@ciseware.com", "Ciseware Automated Emailer");
+            var to = new MailAddress("james@ciseware.com", "James Freiwirth");
+            message.From = from;
+            message.To.Add(to);
+            message.IsBodyHtml = true;
+
+            var smtpMailer = new SmtpClient("mail.ciseware.com", 25);
+            smtpMailer.Send(message);
+        }
+    }
+}
